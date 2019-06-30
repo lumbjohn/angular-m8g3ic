bb826d655208f4dad271a05b924fbc38891560dc
lumbjohn <lumb.john@gmail.com>
2019-06-30 12:46:44 +0300

    8 files modified, 27 added, 4 deleted

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# angular-m8g3ic
+# angular-7-role-based-authorization-example
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-m8g3ic)
+Angular 7 - Role Based Authorization Example with Webpack 4
+
+To see a demo and further details go to http://jasonwatmore.com/post/2018/11/22/angular-7-role-based-authorization-tutorial-with-example
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { routing } from './app.routing';
+
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { HomeComponent } from './home';
+import { AdminComponent } from './admin';
+import { LoginComponent } from './login';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        AdminComponent,
+        LoginComponent
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
